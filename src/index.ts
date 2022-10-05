837dcca9d63cc2cba32341c676b4a4230a46d02a
Nudesuppe42 <jufo.theo@gmail.com>
2022-10-05 21:40:12 +0200

    feat(.): Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import * as bodyParser from "body-parser";
+
+import express, { Request, Response } from "express";
+
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+import helmet from "helmet";
+import logger from "./util/logger";
+import routes from "./routes";
+
+const PORT = process.env.PORT || 8080;
+
+logger.debug("Connecting to database...");
+const prisma = new PrismaClient();
+
+logger.debug("Loading middleware...");
+const app = express();
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(cors());
+
+logger.debug("Registering routes...");
+routes.forEach((route) => {
+  (app as any)[route.method](
+    route.route,
+    async (req: Request, res: Response, next: Function) => {
+      logger.http(`${req.method} ${req.path}$`);
+
+      // TODO: permissions
+
+      const result = new (route.controller as any)()[route.action](
+        req,
+        res,
+        next
+      );
+      if (result instanceof Promise) {
+        result.then((result) =>
+          result !== null && result !== undefined ? res.send(result) : undefined
+        );
+      } else if (result !== null && result !== undefined) {
+        res.json(result);
+      }
+    }
+  );
+});
+app.get("*", (req: Request, res: Response) => {
+  res.send({"test":"test"});
+});
+
+app.listen(PORT, () => {
+    logger.info(`Listening on port ${PORT}`)
+})
---
+++ src/routes/index.ts
@@ -0,0 +1,23 @@
+interface RouteProps {
+    
+    method: string;
+    route: string;
+    controller: any,
+    action: string;
+    permission: any,
+}
+
+
+const routes: RouteProps[] = [
+ /*
+  {
+    method: "",
+    route: "",
+    controller: Controller,
+    action: "",
+    permission: Permissions.default,
+  }
+ */
+];
+
+export default routes;
