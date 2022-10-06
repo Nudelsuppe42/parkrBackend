8330e186587dfa7ee86c1bd10e21ff5224a5f7e5
Nudesuppe42 <jufo.theo@gmail.com>
2022-10-06 21:35:39 +0200

    feat(auth): :sparkles: Added login, and user creation routes. Responses are now sanitized

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -13,9 +13,32 @@ async function createUser(email: string, username: string, password: string) {
     return { error: "Not a valid email address" };
   const hashedPassword = await hash(password);
   const apiKey = await generateApiKey();
-  prisma.user.create({
+  const user = await prisma.user.create({
     data: { email, password: hashedPassword, username, apiKey: apiKey.apiKey },
   });
+  return { error: false, result: sanitize(user) };
+}
+
+async function loginUser(email: string, password: string) {
+  if (
+    !email.match(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  )
+    return { error: "Not a valid email address" };
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) return { error: "No user found" };
+
+  if (await check(password, user.password)) {
+    return {
+      error: false,
+      result: { login: true, user: sanitize(user), apiKey: user.apiKey },
+    };
+  }
+
+  return { error: "Wrong Password" };
 }
 
 async function generateApiKey() {
@@ -28,6 +51,16 @@ async function generateApiKey() {
 async function hash(toHash: string, rounds?: number) {
   return bcrypt.hash(toHash, rounds || 10);
 }
+
 async function check(toCheck: string, hash: string) {
   return bcrypt.compare(toCheck, hash);
 }
+
+function sanitize(toSanitize: any) {
+  var res = JSON.parse(JSON.stringify(toSanitize));
+  res.apiKey = undefined;
+  res.password = undefined;
+  return res;
+}
+
+export default { createUser, loginUser, generateApiKey, hash, check, sanitize };
